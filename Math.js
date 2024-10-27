9c75ce810eeb56c2e4a96e406e56737a52091350
2sjin <options3224@gmail.com>
2024-10-27 23:01:07 +0900

    세미콜론 추가

--- Math.js
+++ Math.js
@@ -1,35 +1,35 @@
 // Math 함수 연습
 
 // 절대값
-console.log(Math.abs(-123))     // 123
+console.log(Math.abs(-123));    // 123
 
 // 제곱(2의 10승)
-console.log(Math.pow(2, 10))    // 1024
+console.log(Math.pow(2, 10));   // 1024
 
 // 올림
 console.log('\n올림')
-console.log(Math.ceil(10.1))    // 11
-console.log(Math.ceil(99.9))    // 100
-console.log(Math.ceil(-10.1))   // -10
-console.log(Math.ceil(-99.9))   // -99
+console.log(Math.ceil(10.1));   // 11
+console.log(Math.ceil(99.9));   // 100
+console.log(Math.ceil(-10.1));  // -10
+console.log(Math.ceil(-99.9));  // -99
 
 // 내림
 console.log('\n내림')
-console.log(Math.floor(10.1))   // 10
-console.log(Math.floor(99.9))   // 99
-console.log(Math.floor(-10.1))  // -11
-console.log(Math.floor(-99.9))  // -100
+console.log(Math.floor(10.1));  // 10
+console.log(Math.floor(99.9));  // 99
+console.log(Math.floor(-10.1)); // -11
+console.log(Math.floor(-99.9)); // -100
 
 // 반올림
 console.log('\n반올림')
-console.log(Math.round(10.1))   // 10
-console.log(Math.round(99.9))   // 100
-console.log(Math.round(-10.1))  // -10
-console.log(Math.round(-99.9))  // -100
+console.log(Math.round(10.1));  // 10
+console.log(Math.round(99.9));  // 100
+console.log(Math.round(-10.1)); // -10
+console.log(Math.round(-99.9)); // -100
 
 // 버림
 console.log('\n버림')
-console.log(Math.trunc(10.1))   // 10
-console.log(Math.trunc(99.9))   // 99
-console.log(Math.trunc(-10.1))  // -10
-console.log(Math.trunc(-99.9))  // -99
+console.log(Math.trunc(10.1));  // 10
+console.log(Math.trunc(99.9));  // 99
+console.log(Math.trunc(-10.1)); // -10
+console.log(Math.trunc(-99.9)); // -99
